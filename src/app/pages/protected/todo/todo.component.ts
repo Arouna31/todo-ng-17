dfdb4b0b29a9fb0423a96462df6810f33d8a9463
Faical KINDA <arounakinda@gmail.com>
2023-12-21 12:13:54 +0100

    create checkbox component

--- src/app/pages/protected/todo/todo.component.ts
+++ src/app/pages/protected/todo/todo.component.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CategoriesNavComponent } from './categories-nav/categories-nav.component';
 import { AddTodoFormComponent } from './add-todo-form/add-todo-form.component';
+import { CheckboxComponent } from '@shared/components/checkbox/checkbox.component';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule, CategoriesNavComponent, AddTodoFormComponent],
+  imports: [
+    CommonModule,
+    CategoriesNavComponent,
+    AddTodoFormComponent,
+    CheckboxComponent,
+  ],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css',
 })
